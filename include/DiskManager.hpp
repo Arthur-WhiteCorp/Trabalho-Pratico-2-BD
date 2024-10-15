3cb673299dfef59fa47ad387710687d8d64f4ba8
Arthur <arthursilvamatias@gmail.com>
2024-10-15 09:30:50 -0400

    adicionado metodos a classe DiskManager

--- include/DiskManager.hpp
+++ include/DiskManager.hpp
@@ -2,19 +2,25 @@
 #define DISK_MANAGER
 
 #include <vector>
+#include <fcntl.h> 
+#include <iostream>
+#include "OperationalSystemDescription.hpp"
 
 class DiskManager{
 private:
+    const char* file_path; // como estou usando baixo nível não posso usar string
+    int arquivo; // file descriptor de baxio nível deferente do file stream
     unsigned int espaco_de_memoria; // em megabytes
-    std::vector <char> vetor_alocacao; // vetor de alocação dos blocos
+    unsigned int tamanho_do_bloco; // em bytes
+    unsigned int quantidade_de_blocos;
+    std::vector <bool> vetor_alocacao; // vetor de alocação dos blocos (true livre) (false alocado)
+    int criarEspacoNaMemoria();
     void setVetorAlocacao();
+    void setQuantidadeDeBlocos();
 
 public:
-    void setEspacoDeMemoria(unsigned int);
     unsigned long long memoryAlloc();
-
-
-    DiskManager(/* args */);
+    DiskManager(const char* file_path, unsigned int espaco_de_memoria, Device* dispositivo_de_memoria);
 };
 
 #endif
--- src/DiskManager.cpp
+++ src/DiskManager.cpp
@@ -1,7 +1,46 @@
 #include "DiskManager.hpp"
+#include <vector>
+#include <fcntl.h> 
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include "OperationalSystemDescription.hpp"
 
-DiskManager::DiskManager(/* args */){
-    
-    vetor_alocacao.assign('l',64000);
+// U significa unsigned int
+#define KILO_BYTE (1024U)
+#define MEGA_BYTE (KILO_BYTE * 1024U)
+
+
+DiskManager::DiskManager(const char* file_path, unsigned int espaco_de_memoria, Device* d_bloco){
+    this->file_path = file_path;
+    this->espaco_de_memoria = espaco_de_memoria;
+    this->tamanho_do_bloco = d_bloco->block_size;
+    criarEspacoNaMemoria();
+    setQuantidadeDeBlocos();
+    setVetorAlocacao();
 }
 
+int DiskManager::criarEspacoNaMemoria(){
+    arquivo = open(file_path, O_RDWR | O_CREAT, 0600);
+    if (arquivo == -1) {
+        std::cerr << "Falha ao abrir o arquivo: " << strerror(errno) << std::endl;
+        return 1;
+    }
+
+    if (fallocate(arquivo, 0, 0, espaco_de_memoria * MEGA_BYTE) != 0) {
+        std::cerr << "Falha ao alocar espaço: " << strerror(errno) << std::endl;
+        close(arquivo);
+        return 1;
+    }
+    std::cout << espaco_de_memoria << "MB alocados" << std::endl;
+
+    return 0;
+}
+
+void DiskManager::setQuantidadeDeBlocos(){
+    quantidade_de_blocos = (espaco_de_memoria * MEGA_BYTE)/tamanho_do_bloco;
+}
+
+void DiskManager::setVetorAlocacao(){
+    vetor_alocacao.assign(quantidade_de_blocos,'l');
+}
